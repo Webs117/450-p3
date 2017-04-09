4acd9261f13dd09e92651e9bed82bf8ce23f2961
webs117 <adamgcog@gmail.com>
2017-04-08 20:14:12 -0400

    greedy done, fcfs update

--- onelanebridge.c
+++ onelanebridge.c
@@ -1,4 +1,3 @@
-
 #define _BSD_SOURCE
 #include <sys/time.h>
 #include <stdio.h>
@@ -8,18 +7,33 @@
 #include <semaphore.h>
 #include <unistd.h>
 
-sem_t bridge;
+sem_t northSem;
+sem_t southSem;
 sem_t dirLock;
-sem_t bridgeLock;
-sem_t empty;
+sem_t carsOnBridgeLock;
+
+sem_t southOpen;
+sem_t northOpen;
+
+sem_t northWait;
+sem_t southWait;
+
+sem_t openBridge;
+
+sem_t switchDir;
 
 //which algorithm to run
-int greedy = 1;
+int greedy = 0;
 
 int direction = 1;
 
 //number of cars of the bridge 
 int carsOnBridge = 0;
+
+//total
+int waiting = 0;
+int waitingNorth = 0;
+int waitingSouth = 0;
 
 double startTime;
 
@@ -54,45 +68,228 @@ void* ArriveBridge(void* carArgs){
 
 	if(greedy == 1){
 		//Run Greedy Crossing
-
-		//if bridge is empty set direction then go
-
-		//if bridge is not empty
-			// if same direction go
-			// if oppostive direction wait
-
-
-
-
-		sem_wait(&bridgeLock);
-		if(carsOnBridge == 0){
-			sem_wait(&dirLock);
-			direction = copyCurrentCar->dir;
-			carsOnBridge++;
-			sem_wait(&empty);
-			//sem_wait(&bridge);
+        
+        //*****************************************************************************************
+        
+        //north and south semaphores start off at 0 (set in main)
+        //if cars == 0
+            //give 3 semaphores to the semaphore that corresponds with the first car's direction
+            
+        //else if cars != 0
+            //wait in the semaphore that corresonds to your direction
+            
+        //******************************************************************************************    
+            
+		sem_wait(&carsOnBridgeLock);
+		if(carsOnBridge == 0){//if cars equal to zero
+		    
+		    //printf("In ArriveBridge (cars == 0), ID: %d, Dir: %d\n", copyCurrentCar->id, copyCurrentCar->dir);
+		   
+		    sem_wait(&dirLock);
+			direction = copyCurrentCar->dir;//change to new direction
 			sem_post(&dirLock);
-		}else{
+		    if(copyCurrentCar->dir == 1){
+		        sem_post(&northSem);
+		        sem_post(&northSem);
+		        sem_post(&northSem);
+		        sem_wait(&northSem);
+		    }
+		    else if(copyCurrentCar->dir == 2){
+		            sem_post(&southSem);
+		            sem_post(&southSem);
+		            sem_post(&southSem);
+		            sem_wait(&southSem);
+		        }
+		    else{printf("Incorrect direction, Direction: %d \n", copyCurrentCar->dir);}
+		    carsOnBridge++;
+		    sem_post(&carsOnBridgeLock);
+		    
+		    
+		}
+		else {//if cars not equal to zero
+		    
+			sem_post(&carsOnBridgeLock);
+		    if(copyCurrentCar->dir == 1){
+	            sem_wait(&northSem);
+	        }
+	        else if(copyCurrentCar->dir == 2){
+	            sem_wait(&southSem);
+	        }
+	        else{printf("Incorrect direction, Direction: %d \n", copyCurrentCar->dir);}
+	        
+		    sem_wait(&carsOnBridgeLock);
+		    carsOnBridge++;
+		    sem_post(&carsOnBridgeLock);
+
+        }
+	}
+	else{
+		//FCFS Crossing
+
+		sem_wait(&carsOnBridgeLock);
+		if(carsOnBridge == 0){//if cars equal to zero
+		    printf("Arriving to Empty Bridge (cars == 0), ID: %d, Dir: %d\n", copyCurrentCar->id, copyCurrentCar->dir);
+		    sem_wait(&dirLock);
+			direction = copyCurrentCar->dir;//change to new direction
+			sem_post(&dirLock);
+		    if(copyCurrentCar->dir == 1){
+		        sem_post(&northSem);
+		        sem_post(&northSem);
+		        sem_post(&northSem);
+		        sem_wait(&northSem);
+		    }
+		    else if(copyCurrentCar->dir == 2){
+		            sem_post(&southSem);
+		            sem_post(&southSem);
+		            sem_post(&southSem);
+		            sem_wait(&southSem);
+		        }
+		    else{printf("Incorrect direction, Direction: %d \n", copyCurrentCar->dir);}
+		    carsOnBridge++;
+		    sem_post(&carsOnBridgeLock);
+		    
+		    
+		}
+		else {//if cars not equal to zero
+		    
+			sem_post(&carsOnBridgeLock);
+
+			//idea 
+			// if 
+
 			sem_wait(&dirLock);
-			if(direction == copyCurrentCar->dir){
-				carsOnBridge++;
-			}else{
-				sem_post(&bridgeLock);
+			if(copyCurrentCar->dir != direction){
+				//cars on bridge are going in different direction
+				
+				//release direction lock
 				sem_post(&dirLock);
-				sem_wait(&empty);
-				sem_wait(&bridgeLock);
-				carsOnBridge++;
+
+
+				//request switch direction since FCFS
+				sem_wait(&switchDir);
+				//increase total waiting cars. Not sure if we need total yet
+				waiting++;
+
+				if(copyCurrentCar->dir == 1){
+					waitingNorth++;
+	        	}
+	        	else if(copyCurrentCar->dir == 2){
+	        		waitingSouth++;
+	        	}				
+				sem_post(&switchDir);
+
+
+	        	//wait until the bridge is open for us to go 
+	        	printf("Time: %2.1f: Vehicle  %d  (%d) Bridge direction is opposite. Tell others to wait and wait until open \n", GetTime()-startTime, copyCurrentCar->id, copyCurrentCar->dir);
+	        	
+
+				//need to wait until direction is avaliable
+				if(copyCurrentCar->dir == 1){
+					//direction = 2 (south)
+					//we want to tell south cars that we are waiting
+	            	sem_wait(&northWait);
+	        	}
+	        	else if(copyCurrentCar->dir == 2){
+	        		//direction = 1 (north)
+					//we want to tell north cars that we are waiting
+	            	sem_wait(&southWait);
+	        	}
+
+
+
+
+				//bridge should be empty now since cars in the same directon should 
+				//			not have crossed because we were waiting 
+
+
 				sem_wait(&dirLock);
-				direction == copyCurrentCar->dir;
+    			direction = copyCurrentCar->dir;//change to new direction
+    			sem_post(&dirLock);
+
+    			if(copyCurrentCar->dir == 1){
+    		        sem_post(&northSem);
+    		        sem_post(&northSem);
+    		        sem_post(&northSem);
+    		        sem_wait(&northSem);
+    		    }
+    		    else if(copyCurrentCar->dir == 2){
+    		            sem_post(&southSem);
+    		            sem_post(&southSem);
+    		            sem_post(&southSem);
+    		            sem_wait(&southSem);
+    		     }
+
+
+			}else{
+				//cars on bridge are going in same direction as us
+
+
+				//idea:
+				//check if another car is waiting on other side
+				//if not go otherwise wait
+
+				//release direction lock 
+				sem_post(&dirLock);
+
+				//check if car on opposite side is waiting
+				sem_wait(&switchDir);
+				if(waiting > 1){
+
+					//we now have to wait until they go other side goes
+					// add ourselves to the openBridge queue 
+					printf("Time: %2.1f: Vehicle  %d  (%d) Same direction but car on otherside waiting \n", GetTime()-startTime, copyCurrentCar->id, copyCurrentCar->dir);
+				
+					//need to wait until direction is avaliable
+					if(copyCurrentCar->dir == 1){
+						//direction = 2 (south)
+						//we want to tell south cars that we are waiting
+		            	sem_wait(&northWait);
+		        	}
+		        	else if(copyCurrentCar->dir == 2){
+		        		//direction = 1 (north)
+						//we want to tell north cars that we are waiting
+		            	sem_wait(&southWait);
+		        	}
+
+
+
+					if(copyCurrentCar->dir == 1){
+		            	sem_wait(&northSem);
+		        	}
+		        	else if(copyCurrentCar->dir == 2){
+		            	sem_wait(&southSem);
+		        	}	
+
+
+				}else{
+
+					//no cars waiting on the other side so cross
+					printf("Time: %2.1f: Vehicle  %d  (%d) Same direction and no cars waiting so lets cross \n", GetTime()-startTime, copyCurrentCar->id, copyCurrentCar->dir);
+					
+
+
+					
+					if(copyCurrentCar->dir == 1){
+		            	sem_wait(&northSem);
+		        	}
+		        	else if(copyCurrentCar->dir == 2){
+		            	sem_wait(&southSem);
+		        	}					
+				}
+				
+
 
 			}
-			sem_post(&dirLock);
-		}
-		sem_post(&bridgeLock);
 
+	        //else{printf("Incorrect direction, Direction: %d \n", copyCurrentCar->dir);}
+	        
+		    sem_wait(&carsOnBridgeLock);
+		    carsOnBridge++;
+		    sem_post(&carsOnBridgeLock);
 
-	}else{
-		//FCFS Crossing
+        }
+
+		
 	}
 
 
@@ -123,16 +320,137 @@ void* ExitBridge(void* carArgs){
 	vehicle_info *copyCurrentCar = (vehicle_info *) carArgs;
 
 	//give back resource (spot on bridge)
-
-	sem_wait(&bridgeLock);
-	carsOnBridge--;
-
-	if(carsOnBridge == 0){
-		sem_post(&empty);
-	}
-	//sem_post(&bridge);
-	sem_post(&bridgeLock);
 	
+	if(greedy == 1){
+	
+    	sem_wait(&dirLock);
+        sem_wait(&carsOnBridgeLock);
+    	
+        if(copyCurrentCar->dir == 1){
+            sem_post(&northSem);
+        }
+        else if(copyCurrentCar->dir == 2){
+            sem_post(&southSem);
+        }
+        else{printf("Incorrect direction, Direction: %d \n", copyCurrentCar->dir);}
+        
+        carsOnBridge--;
+        sem_post(&carsOnBridgeLock);
+        sem_post(&dirLock);
+    	
+    	sem_wait(&carsOnBridgeLock);
+    		if(carsOnBridge == 0){//if cars equal to zero
+    		    sem_wait(&dirLock);
+    			direction = copyCurrentCar->dir;//change to new direction
+    			sem_post(&dirLock);
+    		    if(copyCurrentCar->dir == 2){
+    		        sem_post(&northSem);
+    		        sem_post(&northSem);
+    		        sem_post(&northSem);
+    		    }
+    		    else if(copyCurrentCar->dir == 1){
+    		            sem_post(&southSem);
+    		            sem_post(&southSem);
+    		            sem_post(&southSem);
+    		      }
+    		    else{printf("Incorrect direction, Direction: %d \n", copyCurrentCar->dir);}
+    		    //don't think we need this 
+    		    //carsOnBridge++;
+    		}
+    	    sem_post(&carsOnBridgeLock);
+	}
+	
+	
+	else{
+
+		//FCFS 
+
+
+		sem_wait(&dirLock);
+        sem_wait(&carsOnBridgeLock);
+ 	    sem_wait(&switchDir);
+				
+	    //wait until the bridge is open for us to go 
+	    printf("Time: %2.1f: Vehicle  %d  (%d) in exit lock \n", GetTime()-startTime, copyCurrentCar->id, copyCurrentCar->dir);
+	        	
+		if(waiting != 0){
+			waiting--;
+		}
+				  	
+        if(copyCurrentCar->dir == 1){
+        	if(waitingNorth !=0){
+        		waitingNorth--;
+        	}
+            sem_post(&northSem);
+
+        }
+        else if(copyCurrentCar->dir == 2){
+        	if(waitingSouth !=0){
+        		waitingSouth--;
+        	}
+            sem_post(&southSem);
+        }
+        else{printf("Incorrect direction, Direction: %d \n", copyCurrentCar->dir);}
+        
+        carsOnBridge--;
+        sem_post(&switchDir); 
+        sem_post(&carsOnBridgeLock);
+        sem_post(&dirLock);
+    	
+    	sem_wait(&carsOnBridgeLock);
+    		if(carsOnBridge == 0){//if cars equal to zero
+
+    			//set direction of bridge to direction of last car
+    		    sem_wait(&dirLock);
+    			direction = copyCurrentCar->dir;
+    			sem_post(&dirLock);
+
+
+				sem_wait(&switchDir);
+				if(waiting > 1){
+					printf("I see some cars waiting\n");
+					//need to swap directions to give waiting cars a chance
+					if(copyCurrentCar->dir = 1){
+						//current direction is north so switch direction to south
+						for(int i = 0; i < waitingSouth;i++){
+							//give as many south cars waiting slots to go
+							printf("initialize south\n");
+							sem_post(&southWait);
+						}
+					}else{
+						for(int i = 0; i < waitingSouth;i++){
+							printf("initialize south\n");
+							sem_post(&southWait);
+						}
+					}
+				}
+				sem_post(&switchDir);
+
+
+    			//bridge is now open 
+    			//printf("Time: %2.1f: Vehicle  %d  (%d) Finished crossing and empty bridge. So lets open it up \n", GetTime()-startTime, copyCurrentCar->id, copyCurrentCar->dir);
+    			//sem_post(&openBridge);
+
+    			/*
+    		    if(copyCurrentCar->dir == 2){
+    		        sem_post(&northSem);
+    		        sem_post(&northSem);
+    		        sem_post(&northSem);
+    		    }
+    		    else if(copyCurrentCar->dir == 1){
+    		            sem_post(&southSem);
+    		            sem_post(&southSem);
+    		            sem_post(&southSem);
+    		     }
+    		     */
+    		    //else{printf("Incorrect direction, Direction: %d \n", copyCurrentCar->dir);}
+    		    //don't think we need this 
+    		    //carsOnBridge++;
+    		}
+    	    sem_post(&carsOnBridgeLock);
+
+	    
+	}
 }
 					//generic pointer argument type
 void* VehicleAction(void* carArgs) {
@@ -153,10 +471,18 @@ void* VehicleAction(void* carArgs) {
 int main(int argc, char *argv[]) {
  
 
-	sem_init(&bridge, 1,3);
+	sem_init(&northSem, 1, 0);
+	sem_init(&southSem, 1, 0);
 	sem_init(&dirLock, 0, 1);
-	sem_init(&bridgeLock, 0, 1);
-	sem_init(&empty, 0, 1);
+	sem_init(&carsOnBridgeLock, 0, 1);
+	
+	sem_init(&northWait, 1, 0);
+	sem_init(&southWait, 1, 0);
+
+	sem_init(&openBridge, 1, 0);
+
+
+	sem_init(&switchDir, 1, 1);
 
 	int numCars = 13;
 
@@ -208,4 +534,3 @@ int main(int argc, char *argv[]) {
 
 	return 0;
 }
-
